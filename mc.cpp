583f0ae59bc1ab85dcc6b5f30c622350601b2ae0
Elizabeth Wei <elizabeth.m.wei@gmail.com>
2014-09-29 21:59:16 -0400

    print sol path

--- mc.cpp
+++ mc.cpp
@@ -7,9 +7,9 @@
 using namespace std;
 
 struct state {
-	int m; //num of missionaries on left side
-	int c; //num of cannibals on left side
-	int b; //0 on left side; 1 on right side
+	int m; //num of missionaries on initial side
+	int c; //num of cannibals on initial side
+	bool b; //0 on initial side; 1 on other side
 	vector<state> path; 
 	bool operator==(const state &other) const {
 		return (m == other.m && c == other.c && b == other.b); 
@@ -39,11 +39,10 @@ void genChildren(state &curr_state, bool on_initial_side) {
 				state next_state; 
 				next_state.m = on_initial_side? curr_state.m-i : curr_state.m+i; 
 				next_state.c = on_initial_side? curr_state.c-j : curr_state.c+j;
-				next_state.b = on_initial_side? 1 : 0; 
+				next_state.b = on_initial_side; 
 				if(valid_state(next_state) && visited.find(next_state)==visited.end()) {
-    				printf("NEXT: m-%d c-%d %d\n", next_state.m, next_state.c, next_state.b);
-					for(int p = 0; p < curr_state.path.size(); p++)
-						next_state.path.push_back(curr_state.path[p]);
+    				//printf("NEXT: m-%d c-%d %d\n", next_state.m, next_state.c, next_state.b);
+					next_state.path = curr_state.path; 
 					next_state.path.push_back(curr_state);  
 					q.push(next_state); 
 				}
@@ -56,15 +55,16 @@ int main() {
 
 	q.push(s); 
 	while(!q.empty()) {
-		printf("\n\n\n");
 		state curr_state = q.front(); 
 		q.pop();
 		
 		if(visited.find(curr_state) == visited.end()) { 
-			printf("STATE: m-%d c-%d %d\n", curr_state.m, curr_state.c, curr_state.b);	
+			//printf("STATE: m-%d c-%d %d\n", curr_state.m, curr_state.c, curr_state.b);	
 			
 			if(curr_state.m == 0 && curr_state.b == 0 && curr_state.c == 1) {
 				printf("FOUND SOLUTION\n");
+				for(int i = 0; i < curr_state.path.size(); i++)
+					printf("do move: %d %d %d\n", curr_state.path[i].m, curr_state.path[i].c, curr_state.path[i].b);
 			} 
 
 			visited.insert(curr_state); 
